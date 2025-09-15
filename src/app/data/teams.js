37f05f7803d800af0dcec335c9d0e6a35b049183
Aaron-GF <aarongarcia567@gmail.com>
2025-09-15 11:17:10 +0200

    add teams shields images, complete teams data, resize old images so they are all the same

--- src/app/data/teams.js
+++ src/app/data/teams.js
@@ -1,13 +1,44 @@
 const teams = [
+  // LaLiga
   { name: "Real Madrid", logo: "/teams-shields/real-madrid.png" },
-  { name: "FC Barcelona", logo: "/teams-shields/fc-barcelona.png" },
+  { name: "FC Barcelona", logo: "/teams-shields/barcelona.png" },
+  { name: "Atletico Madrid", logo: "/teams-shields/atletico-madrid.png" },
+
+  // Premier League
   { name: "Liverpool", logo: "/teams-shields/liverpool.png" },
-  { name: "Bayern Munich", logo: "/teams-shields/bayern-munich.png" },
+  { name: "Chelsea", logo: "/teams-shields/chelsea.png" },
+  { name: "Tottenham Hotspur", logo: "/teams-shields/tottenham.png" },
   { name: "Manchester City", logo: "/teams-shields/manchester-city.png" },
   { name: "Arsenal", logo: "/teams-shields/arsenal.png" },
-  { name: "Inter Milan", logo: "/teams-shields/inter-milan.png" },
-  { name: "Atletico Madrid", logo: "/teams-shields/atletico-madrid.png" },
+  { name: "Manchester United", logo: "/teams-shields/manchester-united.png" },
+
+  // Serie A
+  { name: "Juventus", logo: "/teams-shields/juventus.png" },
+  { name: "AC Milan", logo: "/teams-shields/ac-milan.png" },
+  { name: "Napoli", logo: "/teams-shields/napoles.png" },
   { name: "Roma", logo: "/teams-shields/roma.png" },
+  { name: "Inter Milan", logo: "/teams-shields/inter-milan.png" },
+
+  // Bundesliga
+  { name: "RB Leipzig", logo: "/teams-shields/leipzig.png" },
+  { name: "Bayern Munich", logo: "/teams-shields/bayern-munich.png" },
+  { name: "Borussia Dortmund", logo: "/teams-shields/borussia-dortmund.png" },
+
+  // Ligue 1
+  { name: "PSG", logo: "/teams-shields/psg.png" },
+  { name: "Marseille", logo: "/teams-shields/o-marsella.png" },
+  { name: "Monaco", logo: "/teams-shields/monaco.png" },
+  { name: "Lyon", logo: "/teams-shields/o-lyon.png" },
+
+  // Primeira Liga
+  { name: "Porto", logo: "/teams-shields/oporto.png" },
+  { name: "Benfica", logo: "/teams-shields/benfica.png" },
+  { name: "Sporting CP", logo: "/teams-shields/sporting-portugal.png" },
+
+  // Eredivise
+  { name: "Ajax", logo: "/teams-shields/ajax.png" },
 ];
 
 export default teams;
+
+
